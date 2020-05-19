5fbb8422234e9ea3664ad0c14e999bf5b576d325
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-19 10:44:25 -0400

    moved files

--- src/components/products/ProductCrud.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ProductCrud() {
-    return (
-        <div>
-            {/* //This page is for create new product and edit product */}
-        </div>
-    )
-}
