3f8458e90cccfc50f44ac9fffbfad4907345ddb4
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-19 09:55:19 -0400

    initial set up

---
+++ client/src/components/products/Shoes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Shoes() {
+    return (
+        <div>
+            {/* sort by category Shoes here */}
+            Shoes here
+        </div>
+    )
+}
