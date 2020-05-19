ff14cfcd2063e192ff9a522cf5277fa49f65fc0e
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-19 10:43:43 -0400

    created svr added models, cntrlr w/cart/prod schem

---
+++ server/models/product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  id: { type: String, required: true },
+  Category: { type: String, required: true },
+  image: Image,
+  price: { type: String, required: true },
+  brand: String
+});
+
+const Product = mongoose.model("Product", bookSchema);
+
+module.exports = Product;
