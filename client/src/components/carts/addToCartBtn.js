a6b4245dc9c29a05213281863c41b9877900ed6c
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-21 18:37:35 -0400

    remove gitignore files

--- client/src/components/carts/addToCartBtn.js
+++ client/src/components/carts/addToCartBtn.js
@@ -1,12 +1,37 @@
 import React from "react";
 // import "./addToCartBtn.css";
 
-const CartBtn = props => (
-  <button
-    onClick={props.onClick}
-    className={`addToCart ${props["data-value"]}`}
-    {...props}
-  />
-);
+class CartBtn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks:0,
+      show:true
+    };
+  }
+}
+
+IncrementItem = () => {
+  this.setState({ clicks: this.state.clicks + 1 });
+}
+DecreaseItem = () => {
+  this.setState({ clicks: this.state.clicks - 1 });
+}
+ToggleClick = () => {
+  this.setState({ show: !this.state.show });
+}
+
+render() {
+  return (
+    <div>
+      <button onClick={this.IncrementItem}>Click to increment by 1</button>
+      <button onClick={this.DecreaseItem}>Click to decrease by 1</button>
+      <button onClick={this.ToggleClick}>
+        { this.state.show ? 'Hide number' : 'Show number' }
+      </button>
+      { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
+    </div>
+  );
+}
 
 export default CartBtn;
