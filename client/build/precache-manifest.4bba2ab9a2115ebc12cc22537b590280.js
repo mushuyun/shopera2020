83dcd7164fc7fed0e7660d9486605d7e37c85806
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-05-21 21:04:19 -0400

    build

---
+++ client/build/precache-manifest.4bba2ab9a2115ebc12cc22537b590280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c46fbd0db414e2ebd6c3c68b97dd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69cde3df78f741fe4063",
+    "url": "/static/css/main.cbe82ddd.chunk.css"
+  },
+  {
+    "revision": "e65552245f677f30c50b",
+    "url": "/static/js/2.a5321df1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5321df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cde3df78f741fe4063",
+    "url": "/static/js/main.37241839.chunk.js"
+  },
+  {
+    "revision": "56240072e674a518867c",
+    "url": "/static/js/runtime-main.8339eb5f.js"
+  }
+]);
