3a233227e517eda8d160bd4661dd6274fb8340d8
Sue Mu <mushuyun@yahoo.com>
2020-05-26 17:18:37 -0400

    setup user form style

--- client/src/components/users/SignIn.js
+++ client/src/components/users/SignIn.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { signin } from './userActions';
+import "./user.css";
 
 function Signin(props) {
 
