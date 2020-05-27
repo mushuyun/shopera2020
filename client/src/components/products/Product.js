2b68698b991ccc9dea5ab1ad2c6e584aef4d0dd1
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-26 23:31:47 -0400

    Added api call / Local strg to AddToCart

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -2,7 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import data from '../../data/data';
 
+
+
 function Product(props) {
+
+  async function addToCart(productId) {
+    
+    var e = document.getElementById("itemQty");
+
+    if (false) { // user is signed in
+
+      const body = {
+        productId: productId,
+        qty: parseInt(e.options[e.selectedIndex].value)
+      }
+
+      const response = await fetch("/cart", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-type": "application/json"
+        }
+      });
+    } else {
+      let cart = [];
+      if (localStorage.getItem("cart") != null) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      } 
+      let alreadyExists = false;
+      for (var i = 0; i < cart.length; i++) {
+        if (cart[i]["product"]["_id"] == productId) {
+          alreadyExists = true;
+          cart[i]["qty"] += 1;
+          break;
+        } 
+      }
+      if (!alreadyExists) {
+        cart.push({"product": product, "qty": parseInt(e.options[e.selectedIndex].value)});
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  } 
+
+
+
   console.log(props.match.params.id);
   const product = data.products.find(x => x._id === props.match.params.id);
   return <div>
@@ -41,7 +84,7 @@ function Product(props) {
             Status: {product.status}
           </li>
           <li>
-            Qty: <select>
+            Qty: <select id="itemQty">
               <option>1</option>
               <option>2</option>
               <option>3</option>
@@ -50,7 +93,9 @@ function Product(props) {
             </select>
           </li>
           <li>
-            <button className="button primary" id="atc">Add to Cart</button>
+            <button className="button primary" id="atc" onClick={() => addToCart(product._id)}
+            >Add to Cart</button>
+
 
           </li>
         </ul>
