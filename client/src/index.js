696a95157bd2095f6df9d0ef8e497d4906e8095c
Karna <57597042+Karna1014@users.noreply.github.com>
2020-05-27 13:03:12 -0400

    updates to installs

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-=======
 import store from './components/users/store';
->>>>>>> 668cc3b9cb0d745bedd48c79968abc070e6c9d6e
 
 ReactDOM.render(
   <Provider store={store}>
