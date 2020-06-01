f4b277b4d073dc07e0037a8e47a14c4aabb0f87b
Sue Mu <mushuyun@yahoo.com>
2020-06-01 15:06:35 -0400

    editing ProductCrud

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import store from './components/users/store';
 import 'bootstrap/dist/css/bootstrap.css';
