2f7f9d85c05ff214de9e2b4e8b531a012a4c7364
Sue Mu <mushuyun@yahoo.com>
2020-06-01 18:04:00 -0400

    edit crud.css

--- client/src/components/productManage/ProductCrud.js
+++ client/src/components/productManage/ProductCrud.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Home from "../Home.js"
+import "../../styles/crud.css"
 import { saveProduct, listProducts, deleteProdcut } from "./crudActions";
 
 function ProductCrud(props) {
