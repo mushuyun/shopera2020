9ace35e26a1d45e0f6312283af05c08acf73285b
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-06-01 15:13:02 -0400

    update product

--- client/src/components/users/store.js
+++ client/src/components/users/store.js
@@ -1,17 +1,19 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import Cookie from "js-cookie";
-//need to import from cart and product
+//need to import from cart
 import { userSigninReducer, userRegisterReducer, userUpdateReducer } from "./userReducers";
-
+import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from '../products/productReducer';
 //const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
 
 //const initialState = { cart: { cartItems }, userSignin: { userInfo } };
 const initialState = { userSignin: { userInfo } };
 const reducer = combineReducers({
-  //productList: productListReducer,
-  //productDetails: productDetailsReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  productSave: productSaveReducer,
+  productDelete: productDeleteReducer,
   //cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
