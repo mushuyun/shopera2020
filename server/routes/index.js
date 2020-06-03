c97a80093df5591beb264e12f7b77ab0aa0137fa
Karna <57597042+Karna1014@users.noreply.github.com>
2020-06-02 22:26:09 -0400

    removed unused

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const react = require("react");
 const router = require("express").Router;
 const rrd = require("react-router-dom");
-const cartItemController = require("../controller/cartItemController");
+
 
 
 
