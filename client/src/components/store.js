8cf9151d3e8070002330ae871431dd4c41be289e
jhandy4 <58635628+jhandy4@users.noreply.github.com>
2020-06-02 20:36:04 -0400

    update paths

--- client/src/components/store.js
+++ client/src/components/store.js
@@ -1,24 +1,24 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import Cookie from "js-cookie";
-//need to import from cart
-import { userSigninReducer, userRegisterReducer, userUpdateReducer } from "./userReducers";
-import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from '../products/productReducer';
-//const cartItems = Cookie.getJSON("cartItems") || [];
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
+import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from './productManage/crudReducers';
+import { userSigninReducer, userRegisterReducer, userUpdateReducer } from './users/userReducers';
+
+
+const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
 
-//const initialState = { cart: { cartItems }, userSignin: { userInfo } };
 const initialState = { userSignin: { userInfo } };
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  productSave: productSaveReducer,
-  productDelete: productDeleteReducer,
-  //cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
-  userUpdate: userUpdateReducer
+  productSave: productSaveReducer,
+  productDelete: productDeleteReducer,
+  userUpdate: userUpdateReducer,
 })
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
