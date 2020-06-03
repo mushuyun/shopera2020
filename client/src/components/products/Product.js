2a14dda9b18a9227fefb208ad99d62502129ea20
Sue Mu <mushuyun@yahoo.com>
2020-06-03 19:53:50 -0400

    loading products from database

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -1,80 +1,101 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import data from '../../data/data';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from './../productManage/crudActions';
+import { detailsProduct } from './../productManage/crudActions';
+import Home from "../Home";
+import store from "../store";
+import axios from "axios";
+//import data from '../data/data';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function Product(props) {
-
-  async function addToCart(productId) {
     
-    var e = document.getElementById("itemQty");
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector(state => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
 
-    if (false) { // user is signed in
+  useEffect(() => {
+    dispatch(detailsProduct(props.match.params.id));
+    return () => {
+      //
+    };
+  }, []);
+  const handleAddToCart = () => {
+    props.history.push("/cart/" + props.match.params.id + "?qty=" + qty)
+  }
+  // async function addToCart(productId) {
+    
+  //   var e = document.getElementById("itemQty");
 
-      const body = {
-        productId: productId,
-        qty: parseInt(e.options[e.selectedIndex].value)
-      }
+  //   if (false) { // user is signed in
 
-      const response = await fetch("/cart", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-          "Content-type": "application/json"
-        }
-      });
-    } else {
-      let cart = [];
-      if (localStorage.getItem("cart") !== null) {
-        cart = JSON.parse(localStorage.getItem("cart"));
-      } 
-      let alreadyExists = false;
-      for (var i = 0; i < cart.length; i++) {
-        if (cart[i]["product"]["_id"] === productId) {
-          alreadyExists = true;
-          cart[i]["qty"] += 1;
-          break;
-        } 
-      }
-      if (!alreadyExists) {
-        cart.push({"product": product, "qty": parseInt(e.options[e.selectedIndex].value)});
-      }
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-  } 
+  //     const body = {
+  //       productId: productId,
+  //       qty: parseInt(e.options[e.selectedIndex].value)
+  //     }
 
+  //     const response = await fetch("/cart", {
+  //       method: "POST",
+  //       body: JSON.stringify(body),
+  //       headers: {
+  //         "Content-type": "application/json"
+  //       }
+  //     });
+  //   } else {
+  //     let cart = [];
+  //     if (localStorage.getItem("cart") !== null) {
+  //       cart = JSON.parse(localStorage.getItem("cart"));
+  //     } 
+  //     let alreadyExists = false;
+  //     for (var i = 0; i < cart.length; i++) {
+  //       if (cart[i]["product"]["_id"] === productId) {
+  //         alreadyExists = true;
+  //         cart[i]["qty"] += 1;
+  //         break;
+  //       } 
+  //     }
+  //     if (!alreadyExists) {
+  //       cart.push({"product": product, "qty": parseInt(e.options[e.selectedIndex].value)});
+  //     }
+  //     localStorage.setItem("cart", JSON.stringify(cart));
+  //   }
+  // } 
 
-
-  console.log(props.match.params.id);
-  const product = data.products.find(x => x._id === props.match.params.id);
   return <div>
     <div className="back-to-result">
       <Link to="/">Back to result</Link>
     </div>
-    <div className="details">
-      <div className="details-image">
-        <img src={product.image} alt="product" ></img>
-      </div>
-      
-      <div className="details-info">
-        <ul>
-          <li>
-            <h4>{product.name}</h4>
-          </li>
-          <li>
-            {product.rating} Stars ({product.numReviews} Reviews)
-          </li>
-          <li>
-            Price: <b>${product.price}</b>
-          </li>
-          <li>
-            Description:
-            <div>
-              {product.desc}
+    {loading ? <div>Loading...</div> :
+      error ? <div>{error} </div> :
+        (
+          <div className="details">
+            <div className="details-image">
+              <img src={product.image} alt="product" ></img>
             </div>
-          </li>
-        </ul>
-      </div>
-      <div className="details-action">
+            <div className="details-info">
+              <ul>
+                <li>
+                  <h4>{product.name}</h4>
+                </li>
+                <li>
+                  {product.rating} Stars ({product.numReviews} Reviews)
+                </li>
+                <li>
+                  Price: <b>${product.price}</b>
+                </li>
+                <li>
+                  Description:
+                  <div>
+                    {product.desc}
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div className="details-action">
         <ul>
           <li>
             Price: {product.price}
@@ -92,17 +113,16 @@ function Product(props) {
             </select>
           </li>
           <li>
-            <button className="button primary" id="atc" onClick={() => addToCart(product._id)}
-            >Add to Cart</button>
-
-
+            {/* <button className="button primary" id="atc" onClick={() => addToCart(product._id)} */}
+            {/* >Add to Cart</button> */}
+  
           </li>
         </ul>
       </div>
-
-    </div>
+          </div>
+        )
+    }
 
   </div>
 }
-
 export default Product;
