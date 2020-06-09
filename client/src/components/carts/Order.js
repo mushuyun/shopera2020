5627dbe3ca7efab139ffe370c2680dc38e8750e5
Karna <57597042+Karna1014@users.noreply.github.com>
2020-06-09 18:26:35 -0400

    Order Detail Page for Profile/Admin

--- client/src/components/carts/Order.js
+++ client/src/components/carts/Order.js
@@ -1,15 +1,85 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { detailsOrder } from '../orderManage/orderActions';
+import Cookie from "js-cookie";
+import React from "react";
+import { Row, Container, Card, Col } from "reactstrap";
 import "../../styles/order.css";
 
-function Order(props) {
 
-  const orderDetails = useSelector(state => state.orderDetails);
-  const { loading, order, error } = orderDetails;
+class Order extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+ 
+  componentWillMount() {
+    const UserInfo = Cookie.getJSON("userInfo");
+    // const orderId = order._id
+    const orderId = this.props.match.params.id;
+    console.log(orderId);
+    fetch("/api/orders/" + orderId, {
+      method: "GET",
+      headers: { 
+        Authorization: "Bearer " + UserInfo.token 
+      }
+    
+    })
+    .then((response) => response.json())
+    .then((responseJSON) => {
+      console.log(responseJSON);
+      this.setState({data: responseJSON});
+    })
+    .catch((errResp) => {
+      console.log(errResp);
+    });
+  }
 
+  render () {
+    const { data } = this.state;
+    return (
+      <>
+        <Container id="orderCtnr">
+        <Card body outline color="warning">
+            <Row>
+              <Col md={5}>
+                <h1>Buyer: { data.shipping?.firstName } { data.shipping?.lastName }</h1>
+              </Col>
+              <Col md={7}>
+                <h1 id="ordNum">Order#: { this.state.data._id }</h1>
+              </Col>
+            </Row>
+          </Card>
+          <Card body outline color="warning">
+            <h1>Shipping Address</h1>
+              <h2>{ this.state.data.shipping?.streetAddress }</h2>
+              <h2>{ this.state.data.shipping?.aptNumber }</h2>
+              <h2>{ this.state.data.shipping?.cityName }</h2>
+              <h2>{ this.state.data.shipping?.state }</h2>
+              <h2>{ this.state.data.shipping?.zipCode }</h2>
+          </Card>
+          <Card body outline color="warning">
+          
+            <h1>Order Items</h1>
+            <ul className = "oItems" style={{ listStyle: "none" }}> {
+              this.state.data.orderItems?.map(order =>
+              <li key={order._id}>
+              <div className="Purchases">
+                <li>Item: { order.name }</li>
+                <li>Qty Purchased: { order.qty }</li>
+                <li><img className="product-image" src={ order.image } alt="product"/></li> 
+              </div>
+              </li>)
+               }
+             </ul>
+          </Card>
+        </Container>
+      </>
+    );
+};
 }
+
+  
+
 
 
 export default Order;
